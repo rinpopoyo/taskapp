a3aebf2ab9ea71a93ff069c02ca4615650fb9551
rinpopoyo <rinpopoyo@gmail.com>
2015-04-23 05:30:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taskapp::Application.config.secret_key_base = '4dcb650a9db23b314a5256e55b1ce33b785aeecbe3b1e6840b8575be873ce019638632c11dea5b3547d65048a37c8529c86c11535ffed9e7d7311fe0b8d350b1'
